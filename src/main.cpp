e88d213660e5a739f73ab278b2f62e3db084cea5
Rösetta Josephine Kennedy <rosettajosephinekennedy@gmail.com>
2025-10-03 23:26:55 +0100

    Added basic shading

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,58 @@ Model level;
 
 int main() {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    // Sets up the window.
+    // Sets up the window and audio device.
     InitWindow(screenWidth, screenHeight, "Pocket Crew"); SetTargetFPS(60);
-
     InitAudioDevice();
 
+    // Load audio.
     Sound backgroundMusic = LoadSound("assets/music/background.mp3");
-
-    level = LoadModel("assets/models/testmodel.glb");
 
     // Sets up camera values.
     cam.CameraInit();
 
+    // Loads the shader and sets it's values.
+    Shader shader = LoadShader(TextFormat("resources/shaders/lighting.vs", GLSL_VERSION), TextFormat("resources/shaders/lighting.fs", GLSL_VERSION));
+    shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
+    int ambientLoc = GetShaderLocation(shader, "ambient");
+    SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
+
+    // Loads the level model and gives it the shader.
+    level = LoadModel("assets/models/testmodel.glb");
+    level.materials[0].shader = shader;
+    
+    // Create lights.
+    Light light = CreateLight(LIGHT_POINT, (Vector3){ 50.0f, 100.0f, 50.0f }, Vector3Zero(), WHITE, shader);
+
     while (!WindowShouldClose()) {
+        // Toggle borderless fullscreen.
         if (IsKeyPressed(KEY_EQUAL)) { ToggleBorderlessWindowed(); }
+
+        // Background music.
         if (!IsSoundPlaying(backgroundMusic)) { PlaySound(backgroundMusic); }
 
         player.Update();
         cam.Update();
 
+        // Updates the shader camera view vector.
+        float cameraPos[3] = { cam.camera.position.x, cam.camera.position.y, cam.camera.position.z };
+        SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
+
+        // Drawing.
         BeginDrawing();
         ClearBackground(LIGHTGRAY);
-        BeginMode3D(cam.camera);
-            DrawModel(level, (Vector3){ 0.0f, 0.0f, 0.0f }, 1.0f, WHITE);
-            DrawModelWires(level, (Vector3){ 0.0f, 0.0f, 0.0f }, 1.0f, BLACK);
-            DrawSphere(player.position, player.radius, BLUE);
-        EndMode3D();
+            BeginMode3D(cam.camera);
+                BeginShaderMode(shader);
+                    DrawModel(level, Vector3Zero(), 1.0f, WHITE);
+                    DrawSphere(player.position, player.radius, WHITE);
+                EndShaderMode();
+            EndMode3D();
         DrawFPS(10, 10);
         EndDrawing();
     }
 
+    // Unload assets and resources.
+    UnloadShader(shader);
     UnloadModel(level);
     UnloadSound(backgroundMusic);
     CloseAudioDevice();
