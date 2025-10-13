4bf0a5de8a880f0d95c08e1128674bf58ecf9e0d
Rösetta Josephine Kennedy <rosettajosephinekennedy@gmail.com>
2025-10-13 02:12:49 +0100

    Updated build commands

--- build.bash
+++ build.bash
@@ -1,1 +1,1 @@
-gcc -o pocket ./src/*.hpp ./src/*.cpp ./src/levels/*cpp -I ./raylib/include ./raylib/linux/lib/libraylib.a -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o pocket ./src/*.hpp ./src/*.cpp ./src/levels/*.cpp -I ./raylib/include ./raylib/linux/lib/libraylib.a -lGL -lm -lpthread -ldl -lrt -lX11
